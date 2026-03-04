55431671a08518d9eec95499f2dd95b0555d6eae
Koulant <117700254+Koulant@users.noreply.github.com>
2026-03-03 21:51:09 -0700

    chore: set up shadcn, lf eol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -58,6 +60,7 @@ export default function Home() {
           >
             Documentation
           </a>
+          <Button>Button test</Button>
         </div>
       </main>
     </div>
